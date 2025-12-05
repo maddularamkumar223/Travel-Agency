dc65f31889432e9fc699a8367a5d23829d5ff267
maddularamkumar223 <maddularamkumar223@gmail.com>
2025-12-05 15:28:23 +0530

    Login page

---
+++ login/login.js
@@ -0,0 +1,23 @@
+let form = document.querySelector("form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let formData = new FormData(form);
+  let email = formData.get("email");
+  let password = formData.get("password");
+  validation(email, password);
+});
+let validation = async (email, password) => {
+  let response = await fetch("http://localhost:3000/users");
+  let userData = await response.json();
+  let singleUser = userData.find(
+    (value) => value.email == email && value.password == password
+  );
+  if (singleUser) {
+    alert("login Successful");
+    location.href = "../homepage/index.html";
+  } else {
+    alert("Invalid Credentials");
+    location.href = "../registration/register.html";
+  }
+  console.log(singleUser);
+};
---
+++ registration/register.js
@@ -0,0 +1,26 @@
+let form = document.querySelector("form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let formData = new FormData(form);
+  let details = {
+    name: formData.get("name"),
+    contact: formData.get("contact"),
+    email: formData.get("email"),
+    password: formData.get("password"),
+    address: formData.get("address"),
+    gender: formData.get("gender"),
+    dob: formData.get("dob"),
+  };
+  console.log(details);
+  addUsers(details);
+  location.href("../login/login.html");
+});
+let addUsers = async (data) => {
+  await fetch(`http://localhost:3000/users`, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+};
